d0ec99eebdaa6c4518aa5746bccaa3a50ecc567c
Salvatore Polizzotti <salvatore.polizzotti@libero.it>
2023-12-22 12:50:20 +0100

    feat(landing-page):add new features to script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 const subscribeBtn = document.getElementById("subscribe-btn");
+const containerDisplay = document.getElementById("container-display");
+const hiddenSubscribe = document.getElementById("hidden-subscribe");
+const hiddenSubscribeBtn = document.getElementById("hidden-btn");
+const inputBox = document.getElementById("input-box");
 
-function onButtonSubscribe() {}
+function onButtonSubscribe() {
+  let inputBoxValue = inputBox.value;
+  if (inputBoxValue === "") {
+    alert("Please enter your email address!");
+  } else containerDisplay.style.display = "none";
+  hiddenSubscribe.style.display = "flex";
+}
+
+function onButtonReturn() {
+  containerDisplay.style.display = "";
+  hiddenSubscribe.style.display = "none";
+}
+
+subscribeBtn.addEventListener("click", onButtonSubscribe);
+hiddenSubscribeBtn.addEventListener("click", onButtonReturn);
